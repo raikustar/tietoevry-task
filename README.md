8389b77d883f87d21e579d15fb5caa28f8a9b885
raikustar <raikubmx@gmail.com>
2025-06-04 12:52:09 +0300

    .

---
+++ README.md
@@ -0,0 +1,6 @@
+Assignment 1
+
+py -3.12 -m virtualenv env
+.\env\Scripts\activate
+pip install -r .\requirements.txt
+py .\checkurl.py -i .\testfile.csv
---
+++ python/checkurl.py
@@ -0,0 +1,57 @@
+import click
+import csv
+import requests
+
+
+@click.command(
+        context_settings={"help_option_names": ("-h", "--help")},
+        help="Prints out website responses from a csv file. \n " \
+        "Returns website name, status code and time elapsed." )
+@click.option("-i", "--input_file", type=click.Path(exists=True), help="Path to a csv file.")
+@click.option("-t", "--timeout", default=3, help="URL requests timeout threshold in seconds.")
+
+def check_url_from_file(input_file: str, timeout: float):
+    """
+        Prints out website responses from a csv file.
+
+    """
+
+    # function reference
+    check_line = singular_line_parse 
+ 
+    with open(input_file, "r") as file:
+        try:   
+            text_line = csv.reader(file)
+            for line in text_line:
+                result = check_line(current_line=line, time_out=timeout)
+                click.echo(result)
+
+        except Exception as e:
+            click.echo(f"Error: {e}")
+
+
+def singular_line_parse(current_line: list, time_out: float) -> str:
+    """
+        Reads a line from a csv file.
+
+        Splits up name and url from the current line and check the response status code and time elaspsed.
+
+        Returns string with name, status code and time elapsed.
+
+    """
+    try:
+        name, url = "".join(current_line).split("|")
+        response = requests.get(url, timeout=time_out)
+        response_time = round(response.elapsed.total_seconds(), 2)
+
+        return f'"{name}", HTTP {response.status_code}, {response_time} seconds'
+    except Exception as e:
+        if isinstance(e, requests.exceptions.ConnectTimeout):
+            return f'"Skipping {url}"'
+        else:
+            return f'{e}'
+
+
+
+if __name__ == '__main__':
+    check_url_from_file()
